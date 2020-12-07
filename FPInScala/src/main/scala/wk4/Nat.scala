8a83edec69fd28a3d743d491a414d9a266895900
Tcheutchoua Steve <tcheutchouasteve@gmail.com>
2020-12-07 18:24:07 +0100

    exercise

---
+++ FPInScala/src/main/scala/wk4/Nat.scala
@@ -0,0 +1,28 @@
+package wk4
+
+import java.lang.Exception
+
+abstract class Nat {
+  def isZero: Boolean
+  def predecessor: Nat
+  def successor: Nat = new Succ(this)
+  def + (that: Nat): Nat
+  def - (that: Nat): Nat = if (that.isZero) this else (predecessor - that.predecessor)
+}
+
+object Zero extends Nat{
+  override def isZero: Boolean = true
+
+  override def predecessor: Nat = throw new Exception("No predecessor for Zero")
+
+  override def +(that: Nat): Nat = that
+
+}
+
+class Succ(n: Nat) extends Nat{
+  override def isZero: Boolean = false
+
+  override def predecessor: Nat = n
+
+  override def +(that: Nat): Nat = n + that.successor
+}
